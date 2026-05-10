f1e4a1ce2782f925549900697a17ac777faa2abd
Welkie <lamhuynhkhang281@gmail.com>
2026-05-10 22:44:45 +0700

    wsz = 400 swat

--- data/SWAT.py
+++ data/SWAT.py
@@ -35,7 +35,7 @@ class SWAT(Dataset):
         self.data = []
         self.targets = []
         labels = []
-        wsz, stride = 300, 50
+        wsz, stride = 400, 50
 
         if self.train:
             file_path = os.path.join(self.root, "normal.csv")
